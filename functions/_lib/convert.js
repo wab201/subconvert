f8a6ba1beec248b29f0ce803845eaa3f27b5ff6e
Haomuch <Michael.liuhao@gmail.com>
2026-08-04 20:41:31 +0800

    Change default User-Agent to clash-verge/v2.5.1

--- functions/_lib/convert.js
+++ functions/_lib/convert.js
@@ -8,7 +8,7 @@ import { generateSubscription } from './sub-generate.js';
 import { getLink, incrementAccess } from './store.js';
 
 /** Default User-Agent for fetching subscriptions (many providers require a specific UA) */
-const DEFAULT_UA = 'clash';
+const DEFAULT_UA = 'clash-verge/v2.5.1';
 
 /**
  * Fetch source subscription content.
